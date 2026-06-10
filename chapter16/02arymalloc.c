5ae96b1da5394c3e06ceb0a79b48cab0f29121e5
sumin-moon-20251645 <sumin1027@dongyang.ac.kr>
2026-06-10 00:46:42 +0000

    1

--- chapter16/02arymalloc.c
+++ chapter16/02arymalloc.c
@@ -5,34 +5,31 @@
 int main(void)
 {
     int n = 0;
+    int i = 0;
+    int sum = 0;
+    int *ary = NULL;
     printf("입력할 점수의 개수를 입력 >> ");
     scanf("%d", &n);
 
-    int* ary = NULL;
+    ary = (int *)malloc(sizeof(int) * n);
 
-    // 가이드: int형 점수 n개를 저장할 동적 배열을 할당하세요.
-    if ((ary = (int*)[      ]( [           ] * n)) == NULL)
-    {
+    if(ary == NULL) {
         printf("메모리 할당에 문제가 있습니다.");
-        exit(1);
-    };
+        return 1;
+    }
 
     printf("%d개의 점수 입력 >> ", n);
-    int sum = 0;
     for (int i = 0; i < n; i++)
     {
         // 가이드: ary+i 주소에 점수를 입력하고, i번째 값을 sum에 더하세요.
-        scanf("%d", [        ]);
-        sum += [          ];
+        scanf("%d", &ary[i]);
+        sum += ary[i];
     }
 
-    printf("입력 점수: ");
-    for (int i = 0; i < n; i++)
-        printf("%d ", [          ]);
-    printf("\n");
     printf("합: %d  평균: %.1f\n", sum, (double)sum / n);
 
-    [    ](ary);
+    free(ary);
+    ary = NULL;
 
     return 0;
 }
