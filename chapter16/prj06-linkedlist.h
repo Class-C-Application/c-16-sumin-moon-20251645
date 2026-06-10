391e634b33b29f98af458681dd73464231cc836f
sumin-moon-20251645 <sumin1027@dongyang.ac.kr>
2026-06-10 02:29:24 +0000

    5

--- chapter16/prj06-linkedlist.h
+++ chapter16/prj06-linkedlist.h
@@ -1,18 +1,18 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#define _CRT_SECURE_NO_WARNINGS 
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <string.h> 
 
-// 가이드: 문자열 name과 다음 노드 포인터 next를 가진 자기참조 구조체를 정의하세요.
-struct linked_list {
-    char* name;
-    [                   ] next;
-};
+// 자기참조 구조체 정의
+struct linked_list { 
+    char* name; 
+    struct linked_list* next; // 빈칸 1: 다음 노드를 가리키는 자기참조 포인터
+}; 
 
-// 가이드: struct linked_list를 NODE로, NODE*를 LINK로 재정의하세요.
-typedef [                  ] NODE;
-typedef [                  ] LINK;
+// typedef 재정의
+typedef struct linked_list NODE; // 빈칸 2
+typedef struct linked_list* LINK; // 빈칸 3: NODE* 와 같은 의미의 포인터 타입
 
-LINK createNode(char* name);
-LINK append(LINK head, LINK cur);
+LINK createNode(char* name); 
+LINK append(LINK head, LINK cur); 
 int printList(LINK head);
